afbcded0ae1926d12aa91d73dd8851d12d713b1d
Gianpaolo Reinares <gprcards@gmail.com>
2025-04-09 15:19:27 -0700

    Saving for Uploading to Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         <Image src={"/assets/Nvidia.png"} alt='nvidia' width={200} height={30}/>
           </div>
           <div className="px-20 py-5 text-center">
-          <p>NVIDIA pioneered accelerated computing to tackle challenges no one else can solve. Their work in AI and
-          digital twins is transforming the world's largest industries and profoundly impacting society.</p>
+          <p className="font-semibold text-xl">NVIDIA pioneered accelerated computing to tackle challenges no one else can solve. Their work in AI and
+          digital twins is transforming the worlds largest industries and profoundly impacting society.</p>
             <Link href={'#'} className="underline text-blue-600">Job Opportunity</Link>
           </div>
           </div>
@@ -25,7 +25,7 @@ export default function Home() {
         <Image src={"/assets/Intel.png"} alt='intel' width={200} height={30}/>
           </div>
           <div className="px-20 py-5 text-center">
-          <p>Create world-changing technology that improves the life of every person on the planet.
+          <p className="font-semibold text-xl">Create world-changing technology that improves the life of every person on the planet.
             Intel underpins everyday life. We design and manufacture technology that helps us all connect, create, and achieve great things—together.</p>
             <Link href={'#'} className="underline text-blue-600">Job Opportunity</Link>
           </div>
@@ -37,7 +37,7 @@ export default function Home() {
         <Image src={"/assets/Rakuten.png"} alt='rakuten' width={300} height={30} className="p-5"/>
           </div>
           <div className="px-20 py-5 text-center">
-          <p className="font-semibold text-2xl">Since their founding in 1999, Rakuten has helped shape the way people shop online, offering Cash Back, deals and shopping rewards on the world's largest selection of products and services. To date, our 20M+ members in the U.S. have earned over $3.2B in Cash Back at their favorite stores.</p>
+          <p className="font-semibold text-xl">Since their founding in 1999, Rakuten has helped shape the way people shop online, offering Cash Back, deals and shopping rewards on the worlds largest selection of products and services. To date, our 20M+ members in the U.S. have earned over $3.2B in Cash Back at their favorite stores.</p>
           <Link href={'https://rakuten.wd1.myworkdayjobs.com/en-US/RakutenRewards/details/Software-Engineer---Web_1026157?q=frontend'} className="underline text-blue-600">Job Opportunity</Link>
           </div>
         </div>
@@ -48,7 +48,7 @@ export default function Home() {
         <Image src={"/assets/Tesla.png"} alt='tesla' width={300} height={30}/>
           </div>
           <div className="px-20 py-2 text-center">
-          <p>We design sustainable systems that are massively scalable—resulting in the greatest environmental benefit possible. Our energy generation and storage products work together with our electric vehicles to amplify their impact.</p>
+          <p className="font-semibold text-xl">We design sustainable systems that are massively scalable—resulting in the greatest environmental benefit possible. Our energy generation and storage products work together with our electric vehicles to amplify their impact.</p>
           <Link href={'https://www.tesla.com/careers/search/job/internship-backend-software-engineer-diagnostics-fall-2025--240872'} className="underline text-blue-600">Job Opportunity</Link>
           </div>
         </div>
@@ -59,7 +59,7 @@ export default function Home() {
         <Image src={"/assets/Costco.png"} alt='costco' width={300} height={30} className="p-4"/>
           </div>
           <div className="px-20 py-5 text-center">
-            <p>Costco is a membership warehouse club, dedicated to bringing our members the best possible prices on quality brand-name merchandise. With hundreds of locations worldwide, Costco provides a wide selection of merchandise, plus the convenience of specialty departments and exclusive member services, all designed to make your shopping experience a pleasurable one.</p>
+            <p className="font-semibold text-xl">Costco is a membership warehouse club, dedicated to bringing our members the best possible prices on quality brand-name merchandise. With hundreds of locations worldwide, Costco provides a wide selection of merchandise, plus the convenience of specialty departments and exclusive member services, all designed to make your shopping experience a pleasurable one.</p>
             <Link href={'#'} className="underline text-blue-600">Job Opportunity</Link>
           </div>
         </div>
